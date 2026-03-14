187fba2033811b0e4921b35eea0d8dc35066f075
prit007p <pritbaldaniya@gmail.com>
2026-03-14 23:18:08 +0530

    feat: integrate TRPC with React Query and add initial API routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import React, { use } from 'react'
+import { getQueryClient } from '@/trpc/server';
+import { dehydrate, HydrationBoundary, usePrefetchQuery } from '@tanstack/react-query';
+import { Suspense } from 'react';
+import { trpc } from '@/trpc/server';
+import Cleint from './client';
 
-const Page = () => {
- 
+const Page = async () => {
+  const queryClinet = getQueryClient();
+  void queryClinet.prefetchQuery(trpc.hello.queryOptions({ text : "hello"}));
+
   return (
-  <></>
-  )
-}
+    <HydrationBoundary state={dehydrate(queryClinet)}>
+      <Suspense>
+        <Cleint />
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
 
-export default Page
+export default Page;
