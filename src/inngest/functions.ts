56cdb644d5222320adab748d98e7e325376374a7
prit007p <pritbaldaniya@gmai.com>
2026-04-18 17:28:12 +0530

    checking git issue

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -8,10 +8,12 @@ export const helloWorld = inngest.createFunction(
   { event: "test/hello.world" },
   async ({ event, step}) => {
 
+    
     const sandboxId = await step.run("get-sandbox-id",async() => {
       const sandbox = await Sandbox.create("pritbaldaniya/website_builder_template-2");
       return sandbox.sandboxId;
     }); 
+
     const agent = createAgent({
       name: "agent-1",
       system:"you are professional webdeveloper who code clean and simple code in nextjs and react.",
