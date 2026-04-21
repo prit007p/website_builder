c021605dcf2a89693ccdf12630b5810b49f5cc4c
prit007p <pritbaldaniya@gmai.com>
2026-04-21 18:43:59 +0530

    feat: implement message and fragment models with CRUD operations and API integration

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,34 +1,9 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
-import { inngest } from '@/inngest/client';
+
+import { messagesRouter } from '@/modules/messages/server/procedure';
+import {  createTRPCRouter } from '../init';
  
 export const appRouter = createTRPCRouter({
-  invoke: baseProcedure
-    .input(
-      z.object({
-        text:z.string(),
-      }),
-    )
-    .mutation(async ({input}) =>{
-
-      console.log(input);
-
-      await inngest.send({
-        name: "test/hello.world",
-        data: {input: input.text},
-      });
-    }),
-  hello: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      }),
-    )
-    .query((opts) => {
-      return {
-        greeting: `${opts.input.text}`,
-      };
-    }),
+    message:messagesRouter,
 });
  
 export type AppRouter = typeof appRouter;
